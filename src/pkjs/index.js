6462816f28b71984e9972f5e9d8f4763a9b94719
fgiobergia <flavio.giobergia@gmail.com>
2020-04-27 18:04:11 +0200

    sending info

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -5,5 +5,23 @@ Pebble.addEventListener("ready", function(e) {
 
 Pebble.addEventListener("appmessage", function(e) {
 	console.log("Message received");
-	console.log(JSON.stringify(e.payload));
+	const timestampPrefix = e.payload.TimestampPrefix;
+	const keys = Object.keys(e.payload).filter(k => { // filter out TimestampPrefix (and other possible future keys)
+		const key = parseInt(k);
+		return !isNaN(key);
+	});
+	let dict = {};
+	for (const key of keys) {
+		dict[BigInt(key) | (BigInt(timestampPrefix) << BigInt(32))] = e.payload[key].split(",").map(k => parseInt(k));
+	}
+
+	const req = new XMLHttpRequest();
+	req.open("POST", "http://192.168.43.65:5000/api/v1/accel/reading");
+	req.onload = function() {
+		console.log(this.status);
+	};
+	req.setRequestHeader("Content-Type", "application/json");
+	req.send(JSON.stringify(dict));
+
+
 });
