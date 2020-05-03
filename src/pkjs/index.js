a729b0e642907eaf5e97ba6adf24bfb83441eb35
fgiobergia <flavio.giobergia@gmail.com>
2020-05-03 18:49:15 +0200

    added buffering on the phone before sending to the cloud

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,7 +1,11 @@
+const config = require("./config.json");
+
 Pebble.addEventListener("ready", function(e) {
 	console.log("Connected!");
 	Pebble.sendAppMessage({ "JSReady": 1 });
+});
 
+let buffer = {};
 Pebble.addEventListener("appmessage", function(e) {
 	const timestampPrefix = e.payload.TimestampPrefix;
 
@@ -15,14 +19,17 @@ Pebble.addEventListener("appmessage", function(e) {
 		dict[BigInt(key) | (BigInt(timestampPrefix) << BigInt(32))] = e.payload[key].split(",").map(k => parseInt(k));
 	}
 
-	const req = new XMLHttpRequest();
-	const api_endpoint = require("./api.json").api_endpoint;
-	req.open("POST", api_endpoint);
-	req.onload = function() {
-		console.log(this.status);
-	};
-	req.setRequestHeader("Content-Type", "application/json");
-	req.send(JSON.stringify(dict));
-});
+	buffer = Object.assign(buffer, dict);
+
+	if (Object.keys(buffer).length >= config.bufferLength) {
+        const req = new XMLHttpRequest();
+        req.open("POST", config.apiEndpoint);
+        req.onload = function() {
+            console.log(this.status);
+        };
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send(JSON.stringify(buffer));
+        buffer = {};
+    }
 });
 
